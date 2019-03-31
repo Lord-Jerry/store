18c8d0973d8cb561ddc697d61088843facc7debc
Lord Jerry <lovinjerry004@gmail.com>
2019-03-31 21:04:39 +0100

    add some validators to registration route

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,8 +1,25 @@
 const router = require('express').Router();
+const validator = require('../middleware/validator');
+const controller = require('../controllers/user');
 
-router.route('/')
-  .get((req,res) => {
-      res.send('testing router');
-  });
+const url = '/api/v1';
+
+router
+  .route(`${url}/register`)
+  .post(
+    validator.checkBodyContains('first name', 'last name', 'email', 'username', 'gender', 'password', 'password2'),
+    validator.checkBodyNotEmpty('first name', 'last name', 'username', 'email', 'gender', 'password'),
+    validator.checkBodyValidString('first name', 'last name', 'gender'),
+    validator.checkBodyMinValue(3, 'first name', 'last name', 'username', 'password'),
+    validator.checkBodyMinValue(4, 'gender'),
+    validator.checkBodyMaxValue(30, 'first name', 'last name', 'username', 'password'),
+    validator.checkBodyMaxValue(6, 'gender'),
+    validator.checkGenderValid,
+    validator.checkEmailValid,
+    validator.checkUserNameExists,
+    validator.checkEmailExists,
+    validator.checkPasswordsMatch,
+    controller.createAccount,
+  );
 
 module.exports = router;
